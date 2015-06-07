b4f92f794a9118ced142226ea85985e5a6c4148e
Jerry Anselmi <jerry.anselmi@gmail.com>
2015-06-07 18:04:22 -0430

    Ajustess generales y adaptación del codigo a las buenas practicas

--- src/PHPClientAddr/PHPClientAddr.php
+++ src/PHPClientAddr/PHPClientAddr.php
@@ -10,38 +10,154 @@ class PHPClientAddr
      * Lista de posinles variables que contienen la IP del cliente
      * @var array
      */
-    private static $list = array('HTTP_CLIENT_IP','HTTP_X_FORWARDED_FOR','HTTP_X_FORWARDED','HTTP_X_CLUSTER_CLIENT_IP','HTTP_FORWARDED_FOR','HTTP_FORWARDED','REMOTE_ADDR');
+    private $list = array('HTTP_CLIENT_IP','HTTP_X_FORWARDED_FOR','HTTP_X_FORWARDED','HTTP_X_CLUSTER_CLIENT_IP','HTTP_FORWARDED_FOR','HTTP_FORWARDED','REMOTE_ADDR');
+
+    private $private_ip = array(
+        '/^0\./','/^127\.0\.0\.1/',
+        '/^192\.168\..*/','/^172\.((1[6-9])|(2[0-9])|(3[0-1]))\..*/',
+        '/^10\..*/'
+    );
 
     /**
-     * Ip del cliente
+     * Ip del cliente.
      * @var string
      */
-    public static $IP = NULL;
+    public $ip;
 
     /**
-     * Hostname del Cliente
+     * Hostname del Cliente.
      * @var string
      */
-    public static $HOSTNAME = NULL;
+    public $hostname;
 
     /**
-     * Metodo que permite definir los valores de IP y HostName
-     * @return [type] [description]
+     * Arreglo de parametros del servidor.
+     * @var array
      */
-    public static function init()
+    private $server;
+
+    /**
+     * Arreglo de parametros del entorno.
+     * @var array
+     */
+    private $env;
+
+    /**
+     * Constructor
+     * @param array $server Arreglo de valores correspondientes a la variable $_SERVER
+     * @param array $env    Arreglo de valores correspondientes a la variable $_ENV
+     */
+    public function __construct($server = array(), $env = array())
     {
-        foreach (self::$list as $key)
+        $this->server = $server;
+
+        $this->env = $env;
+
+        $this->ip = $this->get_remode_addr();
+
+        $this->get_ip_forwarded();
+
+        $this->hostname = $this->get_remote_hostname();
+    }
+
+    /**
+     * Permite obtener el hostname de la IP
+     * @return string
+     */
+    private function get_remote_hostname()
+    {
+        $hostname = NULL;
+
+        if(!is_null($this->ip))
         {
-            if (array_key_exists($key, $_SERVER) === true) {
-                foreach (explode(',', $_SERVER[$key]) as $ip) {
-                    if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
-                        self::$IP       = $ip;
-                        self::$HOSTNAME = gethostbyaddr($ip);
-                        return ;
-                    }
-                }
-            }            
+            $hostname = gethostbyaddr($this->ip);   
+        }
+        
+        return $hostname;
+    }
+
+    /**
+     * Permite obtener la IP proveniente de un servidor proxy
+     * @return void
+     */
+    private function get_ip_forwarded()
+    {
+        if(!!$this->is_http_x_forwarded_for($this->server))
+        {
+            $entries = get_http_x_forwarded_for_entities($this->server);
+
+            $this->ip = $this->get_x_forwarded_ip($entries);          
         }
     }
+
+    /**
+     * Permite saber si la peticion proviene de un servidor proxy.
+     * @return boolean
+     */
+    private function is_http_x_forwarded_for()
+    {
+        return !!isset($this->server['HTTP_X_FORWARDED_FOR'])&&$this->server['HTTP_X_FORWARDED_FOR']!=''; 
+    }
+
+    /**
+     * Permite obtener todas las entidades enviadas por un servidor proxy. 
+     * @return array
+     */
+    private function get_http_x_forwarded_for_entities()
+    {
+        $entries = split('[, ]', $this->server['HTTP_X_FORWARDED_FOR']);
+        reset($entries);
+        return $entries;
+    }
+
+    /**
+     * Permite obtener la IP real proveniente de un servidor proxy.
+     * @param  array $entries Arreglo de entidades enviadas por un servidor proxy
+     * @return string
+     */
+    private function get_x_forwarded_ip($entries)
+    {
+        $ip = $this->ip;
+
+        while (list(, $entry) = each($entries))
+        {
+            $entry = trim($entry);
+            if ( preg_match("/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/", $entry, $ip_list))
+            {
+                $found_ip = preg_replace( $this->private_ip, $ip, $ip_list[1]);
+     
+                if ($ip != $found_ip)
+                {
+                    $ip = $found_ip;
+                    break;
+                }
+            }
+        }
+
+        return $ip;
+    }
+
+    /**
+     * Permite obtener la IP real del cliente.
+     * @return string
+     */
+    private function get_remode_addr()
+    {
+        $ip = NULL;
+
+        if(PHP_SAPI=='cli')
+        {
+            $ip = getHostByName(getHostName());
+        }
+        else if(!!isset($this->server['REMOTE_ADDR'])&&!empty($this->server['REMOTE_ADDR']))
+        {
+            $ip = $this->server['REMOTE_ADDR'];
+        }
+        else if(!!isset($this->env['REMOTE_ADDR'])&&!empty($this->env['REMOTE_ADDR']))
+        {
+            $ip = $this->env['REMOTE_ADDR'];
+        }
+
+        return $ip;
+    }
 }
-PHPClientAddr::init();
